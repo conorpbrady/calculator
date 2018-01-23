d9b6cc2b1efaa5671f544e27c710ad33a1adbadc
Conor Brady <conorpbrady@gmail.com>
2018-01-23 15:50:48 -0800

    Init and add basic structure

---
+++ calc.js
@@ -0,0 +1,26 @@
+//calc.js
+
+function operate(func,a,b) {
+  return func(a,b);
+}
+
+function add(a,b) {
+  return a+b;
+}
+
+function sub(a,b) {
+
+}
+
+function mul(a,b) {
+
+}
+
+function div(a,b) {
+
+}
+
+
+
+//1+1
+console.log(operate(add,1,1));
