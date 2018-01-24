d133b47be31c1cffa1ace87e954ef5e5dcfda86b
Conor Brady <conorpbrady@gmail.com>
2018-01-24 14:31:36 -0800

    Add event listeners

--- calc.js
+++ calc.js
@@ -20,7 +20,21 @@ function div(a,b) {
 
 }
 
-
+let buttonPress = (e) => {
+	pressed = e.target.id;
+	display.textContent += e.target.id;
+}
 
 //1+1
 console.log(operate(add,1,1));
+
+
+
+
+let display = document.querySelector("#display");
+
+var btns = document.querySelectorAll(".num, .op");
+
+btns.forEach( btn => {
+	btn.addEventListener('click', buttonPress);
+});
